35ffe8657304945d7470c2401d6ac1dbb432b0d5
AJLobo <adrian.abeyta16@gmail.com>
2024-03-12 00:58:51 -0600

    havent tested still

--- scripts/cs481/homework_flow/homework_flow.sh
+++ scripts/cs481/homework_flow/homework_flow.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+git checkout homework$1
+mkdir build
+
+git submodule init
+git submodule update --remote
+
+cd build
+cmake ..
+
+cd ..
+git checkout main
+git merge homework$1 --allow-unrelated-histories
+git commit -m "merged homework$1"
+git push
+
